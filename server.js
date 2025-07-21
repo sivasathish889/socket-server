c193f4b54463b7af7b5e08d6552a33ce49487345
sivasathish889 <sivasathish889@gmail.com>
2025-07-21 18:00:31 +0530

    update socket

--- server.js
+++ server.js
@@ -4,30 +4,38 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const { verify } = require("jsonwebtoken");
+const dbServerCon = require("./db.js");
+const ConservationModel = require("./ConservationModel.js");
+
+// env configuration
 require("dotenv").config();
+
 const app = express();
 const server = createServer(app);
-const ConservationModel = require("./ConservationModel.js");
-const dbServerCon = require("./db.js");
 let serverUrl = process.env.SERVER_URL || "localhost";
 
+// Database connection
+dbServerCon();
+
+// serverURL configuration
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://chat-application-9hs7-git-master-sivasathish889s-projects.vercel.app/","http://localhost:3000"],
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
     credentials: true,
   })
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-dbServerCon();
 app.get("/", (req, res) => {
   res.send("Socket server is running");
 });
+
+// Socket.io configuration
 const io = new Server(server, {
   cors: {
-    origin: ["https://chat-application-9hs7-git-master-sivasathish889s-projects.vercel.app/", "http://localhost:3000"],
+    origin: [process.env.CLIENT_URL, "http://localhost:3000"],
     methods: ["GET", "POST"],
     allowedHeaders: ["Content-Type", "Headers"],
     credentials: true,
@@ -36,6 +44,7 @@ const io = new Server(server, {
 });
 
 let users = {};
+// Store connected users
 io.on("connection", (socket) => {
   if (socket.handshake.headers.cookie) {
     const cookies = socket.handshake.headers.cookie
